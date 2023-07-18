40cf0db5f120ee212e322fe4bca2476c0816482b
Jalil Nouri <nouri.6412@gmail.com>
2023-07-18 10:44:16 +0330

    123

---
+++ model/invoice.js
@@ -0,0 +1,351 @@
+const header = {
+     Taxid:
+    {
+        required: true,
+        title: 'Taxid'
+    },
+     Indatim:
+    {
+        required: false,
+        title: 'Indatim'
+    },
+    Indati2m:
+    {
+        required: false,
+        title: 'Indati2m'
+    },
+    Inty:
+    {
+        required: false,
+        title: 'Inty'
+    },
+    Inno:
+    {
+        required: false,
+        title: 'Inno'
+    },
+    Irtaxid:
+    {
+        required: false,
+        title: 'Irtaxid'
+    },
+    Inp:
+    {
+        required: false,
+        title: 'Inp'
+    },
+    Ins:
+    {
+        required: false,
+        title: 'Ins'
+    },
+    Tins:
+    {
+        required: false,
+        title: 'Tins'
+    },
+    Tob:
+    {
+        required: false,
+        title: 'Tob'
+    },
+    Bid:
+    {
+        required: false,
+        title: 'Bid'
+    },
+    Tinb:
+    {
+        required: false,
+        title: 'Tinb'
+    },
+    Sbc:
+    {
+        required: false,
+        title: 'Sbc'
+    },
+    Bpc:
+    {
+        required: false,
+        title: 'Bpc'
+    },
+    Bbc:
+    {
+        required: false,
+        title: 'Bbc'
+    },
+    Ft:
+    {
+        required: false,
+        title: 'Ft'
+    },
+    Bpn:
+    {
+        required: false,
+        title: 'Bpn'
+    },
+    Scln:
+    {
+        required: false,
+        title: 'Scln'
+    },
+    Scc:
+    {
+        required: false,
+        title: 'Scc'
+    },
+    Crn:
+    {
+        required: false,
+        title: 'Crn'
+    },
+    Billid:
+    {
+        required: false,
+        title: 'Billid'
+    },
+    Tprdis:
+    {
+        required: false,
+        title: 'Tprdis'
+    },
+    Tdis:
+    {
+        required: false,
+        title: 'Tdis'
+    },
+    Tadis:
+    {
+        required: false,
+        title: 'Tadis'
+    },
+    Tvam:
+    {
+        required: false,
+        title: 'Tvam'
+    },
+    Todam:
+    {
+        required: false,
+        title: 'Todam'
+    },
+    Tbill:
+    {
+        required: false,
+        title: 'Tbill'
+    },
+    Setm:
+    {
+        required: false,
+        title: 'Setm'
+    },
+    Cap:
+    {
+        required: false,
+        title: 'Cap'
+    },
+    Insp:
+    {
+        required: false,
+        title: 'Insp'
+    },
+    Tvop:
+    {
+        required: false,
+        title: 'Tvop'
+    },
+    Tax17:
+    {
+        required: false,
+        title: 'Tax17'
+    },
+    Cdcn:
+    {
+        required: false,
+        title: 'Cdcn'
+    },
+    Cdcd:
+    {
+        required: false,
+        title: 'Cdcd'
+    },
+    Tonw:
+    {
+        required: false,
+        title: 'Tonw'
+    },
+    Torv:
+    {
+        required: false,
+        title: 'Torv'
+    },
+    Tocv:
+    {
+        required: false,
+        title: 'Tocv'
+    }
+};
+
+const body = {
+    Sstid: {
+        required: false,
+        title: 'Sstid'
+    },
+    Sstt: {
+        required: false,
+        title: 'Sstt'
+    },
+    Mu: {
+        required: false,
+        title: 'Mu'
+    },
+    Am: {
+        required: false,
+        title: 'Am'
+    },
+    Fee: {
+        required: false,
+        title: 'Fee'
+    },
+    Cfee: {
+        required: false,
+        title: 'Cfee'
+    },
+    Cut: {
+        required: false,
+        title: 'Cut'
+    },
+    Exr: {
+        required: false,
+        title: 'Exr'
+    },
+    Prdis: {
+        required: false,
+        title: 'Prdis'
+    },
+    Dis: {
+        required: false,
+        title: 'Dis'
+    },
+    Adis: {
+        required: false,
+        title: 'Adis'
+    },
+    Vra: {
+        required: false,
+        title: 'Vra'
+    },
+    Vam: {
+        required: false,
+        title: 'Vam'
+    },
+    Odt: {
+        required: false,
+        title: 'Odt'
+    },
+    Odr: {
+        required: false,
+        title: 'Odr'
+    },
+    Odam: {
+        required: false,
+        title: 'Odam'
+    },
+    Olt: {
+        required: false,
+        title: 'Olt'
+    },
+    Olr: {
+        required: false,
+        title: 'Olr'
+    },
+    Olam: {
+        required: false,
+        title: 'Olam'
+    },
+    Consfee: {
+        required: false,
+        title: 'Consfee'
+    },
+    Spro: {
+        required: false,
+        title: 'Spro'
+    },
+    Bros: {
+        required: false,
+        title: 'Bros'
+    },
+    Tcpbs: {
+        required: false,
+        title: 'Tcpbs'
+    },
+    Cop: {
+        required: false,
+        title: 'Cop'
+    },
+    Vop: {
+        required: false,
+        title: 'Vop'
+    },
+    Bsrn: {
+        required: false,
+        title: 'Bsrn'
+    },
+    Tsstam: {
+        required: false,
+        title: 'Tsstam'
+    },
+    Nw: {
+        required: false,
+        title: 'Nw'
+    },
+    Ssrv: {
+        required: false,
+        title: 'Ssrv'
+    },
+    Sscv: {
+        required: false,
+        title: 'Sscv'
+    }
+};
+
+const payment = {
+    Iinn: {
+        required: false,
+        title: 'Iinn'
+    },
+    Acn: {
+        required: false,
+        title: 'Acn'
+    },
+    Trmn: {
+        required: false,
+        title: 'Trmn'
+    },
+    Trn: {
+        required: false,
+        title: 'Trn'
+    },
+    Pcn: {
+        required: false,
+        title: 'Pcn'
+    },
+    Pid: {
+        required: false,
+        title: 'Pid'
+    },
+    Pdt: {
+        required: false,
+        title: 'Pdt'
+    },
+    Pmt: {
+        required: false,
+        title: 'Pmt'
+    },
+    Pv: {
+        required: false,
+        title: 'Pv'
+    }
+};
+
+module.exports = { header, body, payment };
