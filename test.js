3262f2b41131e384e05d5149468c5895549ebbb0
Jalil Nouri <nouri.6412@gmail.com>
2023-07-19 13:10:21 +0330

    123

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
 const axios = require('axios');
 axios.post('http://localhost:8181/invoice/send_invoice', {init_params:{client_id:'aa',private_key:'11'},invoice:{
-    header:{},body:[],payments:[]
+    header:{
+        bb:'b_h',
+        aa:'',
+        za:'z_h'
+    },body:[
+        {
+            cc:'c_b',
+            aa:null,
+            za:'z_b'
+        },
+        {
+            cc:'c1_b',
+            aa:'',
+            za:'z1_b'
+        }
+    ],payments:[]
 }})
     .then(response => {
         console.log(response.data);
--- utils/signatory.js
+++ utils/signatory.js
@@ -1,8 +1,7 @@
 var middlewareObj = {};
 
-function sort_json(json)
-{
-    var  ordered = Object.keys(json).sort().reduce(
+function sort_json(json) {
+    var ordered = Object.keys(json).sort().reduce(
         (obj, key) => {
             obj[key] = json[key];
             return obj;
@@ -14,28 +13,51 @@ function sort_json(json)
 }
 
 function normalize(json) {
-  
-    var str = '';
 
-    var  ordered =sort_json(json);
+    var normalize_str = '';
+
+    var ordered = sort_json(json);
 
     const props = Object.keys(ordered);
-    for (var x = 0; x < props.length; x++)
-    {
-       if(ordered[props[x]].length)
-       {
-        for (var y = 0; y < ordered[props[x]].length; y++)
-        {
-            ordered[props[x]][y]=sort_json(ordered[props[x]][y]);
+    var sep = '';
+
+    for (var x = 0; x < props.length; x++) {
+        if (ordered[props[x]].length) {
+            for (var y = 0; y < ordered[props[x]].length; y++) {
+                ordered[props[x]][y] = sort_json(ordered[props[x]][y]);
+
+                var sub_props = Object.keys(ordered[props[x]][y]);
+                for (var i = 0; i < sub_props.length; i++) {
+
+                    var val='#';
+                    if(ordered[props[x]][y][sub_props[i]])
+                    {
+                        val =ordered[props[x]][y][sub_props[i]];
+                    }
+
+                    normalize_str = normalize_str + sep + val;
+                    sep = '#';
+                }
+            }
         }
-       }
-       else
-       {
-        ordered[props[x]]=sort_json(ordered[props[x]]);
-       }
+        else {
+            ordered[props[x]] = sort_json(ordered[props[x]]);
+            var sub_props = Object.keys(ordered[props[x]]);
+            for (var i = 0; i < sub_props.length; i++) {
+
+                var val='#';
+                if(ordered[props[x]][sub_props[i]])
+                {
+                    val =ordered[props[x]][sub_props[i]];
+                }
+               
+                normalize_str = normalize_str + sep + val;
+                sep = '#';
+            }
+        }
     }
 
-    return str;
+    return normalize_str;
 }
 
 middlewareObj.signatory = function (json) {
