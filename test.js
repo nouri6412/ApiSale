666f28dd49c8b577844d69514f6ee60fc018ee4f
Jalil Nouri <nouri.6412@gmail.com>
2023-07-23 11:56:09 +0330

    4444

--- test.js
+++ test.js
@@ -35,7 +35,7 @@ try {
   console.error(err);
 }
 //console.log(pk);
-axios.post('http://localhost:8181/invoice/send_invoice', {init_params:{client_id:'A1211P',private_key:pk},invoice:{
+axios.post('http://torgay.ir/project/send_invoice', {init_params:{client_id:'A1211P',private_key:pk},invoice:{
     header:{
         bb:'b_h',
         aa:'a#h',
